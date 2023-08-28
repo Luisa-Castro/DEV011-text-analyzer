1a28d5df4387fb6eb15f67a6265ecb83316ce43c
Luisa Castro <luisa.castroa@hotmail.com>
2023-08-28 14:52:29 -0500

    mi primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import analyzer from './analyzer.js';
 
+console.log("Hola");
+
+//Obtener elementos del DOM
+const userInput = document.querySelector("textarea[name='user-input']");
+const output = document.querySelector("#output");
+userInput.addEventListener("input", (event) => {
+    output.textContent = `Presionaste "${event.target.value}".`
+    console.log(userInput.value);
+});
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
