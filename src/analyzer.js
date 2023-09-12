06d6c80c6f22a0050c2340206384721e99d945c5
Luisa-Castro <luisa.castroa@hotmail.com>
2023-09-12 13:32:42 -0500

    prueba readme

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,8 +1,7 @@
 const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-
-    const textsplit = text.split(" ");
+    const textsplit = text.replaceAll(/[.,#!¡$%&*;:«»[{}=\-_`~()¿?]/g, "").split(" ");
     const word_num = textsplit.filter(Boolean).length;
     return word_num;
   },
@@ -13,8 +12,8 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    const textsinesp = text.replaceAll(/[.,#!$%&*;:{}=\-_`~()]/g, "");
-    const Character_Excluding_Spaces = textsinesp.replaceAll(" ", "").length;
+    const textexcludingspaces = text.replaceAll(/[.,#!$%&*;:{}=\-_`~()]/g, "");
+    const Character_Excluding_Spaces = textexcludingspaces.replaceAll(" ", "").length;
     return Character_Excluding_Spaces;
 
   },
@@ -34,12 +33,12 @@ const analyzer = {
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    const textsplit = text.split(" ");
-    const textfilter = textsplit.filter(Boolean);
+    const textsplit = text.replaceAll(/[,#!$%&*;:{}=\-_`~()]/g, "").split(" ").filter(Boolean);
+    //const textfilter = textsplit.filter(Boolean);
     let Number_Count = 0;
 
-    for (let i = 0; i < textfilter.length; i++) {
-      if (!isNaN(textfilter[i])) {
+    for (let i = 0; i < textsplit.length; i++) {
+      if (!isNaN(textsplit[i])) {
         Number_Count = Number_Count + 1;
       }
     }
@@ -47,7 +46,7 @@ const analyzer = {
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    const textsplit = text.split(" ").filter(Boolean);
+    const textsplit = text.replaceAll(/[,#!$%&*;:{}=\-_`~()]/g, "").split(" ").filter(Boolean);
     let Number_Sum = 0;
 
     for (let i = 0; i < textsplit.length; i++) {
